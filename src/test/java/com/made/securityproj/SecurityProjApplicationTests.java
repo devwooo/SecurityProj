e16269a4e290aa6b85bb9f65fcb891df4f90de18
devwooo <devacnt25@gmail.com>
2025-05-12 12:26:18 +0900

    [시큐리티 프로젝트] 첫 커밋

---
+++ src/test/java/com/made/securityproj/SecurityProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.made.securityproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
