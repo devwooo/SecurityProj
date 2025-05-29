9ae3d000ccf362f882ef1b48fadfbfca91f5137f
kyongwoo <rineee@easymedia.net>
2025-05-29 17:01:42 +0900

    Bootstrap admin template 적용

---
+++ src/main/java/com/made/securityproj/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.made.securityproj.config;
+
+import com.made.securityproj.interceptor.AuthInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor).addPathPatterns("/pages/**");
+    }
+
+}
---
+++ src/main/java/com/made/securityproj/interceptor/AuthInterceptor.java
@@ -0,0 +1,17 @@
+package com.made.securityproj.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class AuthInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        /* Interceptor 에 대한 내용이 들어간다. */
+
+        return true;
+    }
+}
